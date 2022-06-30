e3fc907c05b169da1f8f2cdec839c51b2dcc8767
youngsun <dudtjs1021@hansung.ac.kr>
2022-06-30 16:54:52 +0900

    CodeUp - 21,31

---
+++ CodeUp/21.swift
@@ -0,0 +1,11 @@
+//
+//  21.swift
+//  Algorithm
+//
+//  Created by 임영선 on 2022/06/30.
+//
+
+import Foundation
+
+let input = readLine()!
+print(input)
---
+++ CodeUp/31.swift
@@ -0,0 +1,13 @@
+//
+//  31.swift
+//  Algorithm
+//
+//  Created by 임영선 on 2022/06/30.
+//
+
+import Foundation
+
+// 10진수 -> 8진수
+let input = Int(readLine()!)!
+let binary = String(input, radix: 8)
+print(binary)
