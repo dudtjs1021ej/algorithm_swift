46c82213f1a543d427b6ec076c99bff4e08fb4dc
youngsun <dudtjs1021@hansung.ac.kr>
2022-07-06 17:09:48 +0900

    Basic - Queue 기본

---
+++ Basic/Queue.swift
@@ -0,0 +1,36 @@
+//
+//  Queue.swift
+//  Algorithm
+//
+//  Created by 임영선 on 2022/07/06.
+//
+
+import Foundation
+
+public struct Queue<T> {
+  fileprivate var array = [T]()
+
+  public var isEmpty: Bool {
+    return array.isEmpty
+  }
+  
+  public var count: Int {
+    return array.count
+  }
+
+  public mutating func enqueue(_ element: T) {
+    array.append(element)
+  }
+  
+  public mutating func dequeue() -> T? {
+    if isEmpty {
+      return nil
+    } else {
+      return array.removeFirst()
+    }
+  }
+  
+  public var front: T? {
+    return array.first
+  }
+}
