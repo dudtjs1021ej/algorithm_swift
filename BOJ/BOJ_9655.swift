2c994544c0c46c44c75c6c78e5176b182a24b70a
youngsun <dudtjs1021@hansung.ac.kr>
2022-08-22 00:23:22 +0900

    BOJ 9655 - 돌 게임 : DP

---
+++ BOJ/BOJ_9655.swift
@@ -0,0 +1,16 @@
+//
+//  BOJ_9655.swift
+//  Algorithm
+//
+//  Created by 임영선 on 2022/08/22.
+//  BOJ 9655 - 돌 게임 : DP
+import Foundation
+
+let n = Int(readLine()!)!
+if n%2==0 {
+  print("CY")
+}
+else {
+  print("SK")
+}
+
